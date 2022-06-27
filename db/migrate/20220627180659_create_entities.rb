10a9ab3ef43180615681b6551d7ac85a4d8aa43f
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-27 21:09:53 +0300

    entity migration

---
+++ db/migrate/20220627180659_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.integer :authorid
+      t.string :name
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end
