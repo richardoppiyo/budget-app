2b49af48ada6f7ecf459572e2e5fff660cb3a28f
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-27 21:12:40 +0300

    fix rubocop isssues

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_27_180659) do
+ActiveRecord::Schema[7.0].define(version: 20_220_627_180_659) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "entities", force: :cascade do |t|
-    t.integer "authorid"
-    t.string "name"
-    t.float "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'entities', force: :cascade do |t|
+    t.integer 'authorid'
+    t.string 'name'
+    t.float 'amount'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'groups', force: :cascade do |t|
+    t.string 'name'
+    t.string 'icon'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.string "index"
-    t.string "show"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'homes', force: :cascade do |t|
+    t.string 'index'
+    t.string 'show'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "index"
-    t.string "show"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'index'
+    t.string 'show'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
