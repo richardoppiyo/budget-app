59217dd6c2f77aea17898693c0c6eea733a15f9c
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-27 22:05:54 +0300

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_185716) do
     t.string "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_entities_on_user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -45,6 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_27_185716) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "entities", "users"
+  add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "groups", "users"
 end
