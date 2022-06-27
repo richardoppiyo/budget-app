b63ee0bebcbc146487a1960398998b7e2ebe379e
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-28 02:24:43 +0300

    change groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,7 @@ class GroupsController < ApplicationController
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = current_user.Group.new(group_params)
 
     respond_to do |format|
       if @group.save
