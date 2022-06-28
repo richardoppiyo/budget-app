a4ff8061c96553735a9045e29164114c23483dba
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-29 02:18:55 +0300

    modify model to accomodate many to many associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :transactions
   has_many :entities, through: :transactions
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
